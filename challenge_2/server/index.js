beadf4cd4a71e7002ef374614d1c37bb680802a8
Steve Hallam <StevenAHallam@gmail.com>
2018-04-05 12:09:08 -0700

    Implemented a new react app using create-react-app

---
+++ challenge_2/server/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 5000;
+
+app.use('/', express.static(path.join(__dirname, 'build')));
+
+app.listen(port, () => console.log(`Checkout listening on port ${port}!`));
